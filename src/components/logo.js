413d2d2c4af4b55d3be4308c0365c6c93fc7ee30
NinaBalan <52449291+NinaBalan@users.noreply.github.com>
2020-10-19 22:38:25 +0300

    insert logo

---
+++ src/components/logo.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Logo extends Component {
+    render() {
+        return (
+            <div className="logo-main">
+                <img alt="my cookbook image logo " src="/assets/images/cookbook.png" />
+            </div>
+        )
+    }
+}
+
+export default Logo;
